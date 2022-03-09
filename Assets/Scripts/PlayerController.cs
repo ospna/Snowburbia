ac6676f39408d31e61c271068fa728cd5492ceea
Giovanny Ospina <giovosp@gmail.com>
2022-03-09 14:38:19 -0500

    Switching over to 2.5D again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private Animator animator;
+    private AudioSource audioSrc;
 
     public Vector3 direction;
 
-    public float speed = 3;
+    public float speed =3 ;
     public float jumpForce = 10;
     public float gravity = -20;
+    public float inputMagnitude;
 
     public Transform groundCheck;
     public LayerMask groundLayer;
     public Transform model;
+
+    private bool isGrounded;
+    private bool isJumping;
+    private bool isThrowing;
+    private bool isMoving;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -34,28 +43,46 @@ public class PlayerController : MonoBehaviour
         bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
         direction.y += gravity * Time.deltaTime;
 
-        if(isGrounded)
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, 0);
+
+        // slow down
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            inputMagnitude /= 2;
+        }
+
+        if (isGrounded)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 direction.y = jumpForce;
                 animator.SetBool("isJumping", true);
+                isJumping = true;
+
                 animator.SetBool("isGrounded", false);
+                isGrounded = false;
             }
             else
             {
                 animator.SetBool("isJumping", false);
+                isJumping = false;
+
                 animator.SetBool("isGrounded", true);
+                isGrounded = true;
             }
         }
 
         if (Input.GetButton("Fire1") || Input.GetAxis("Fire1") != 0)
         {
             animator.SetBool("isThrowing", true);
+            isThrowing = true;
         }
         else
         {
             animator.SetBool("isThrowing", false);
+            isThrowing = false;
         }
 
         controller.Move(direction * Time.deltaTime);
@@ -63,12 +90,25 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput != 0)
         {
             animator.SetBool("isMoving", true);
+            isMoving = true;
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(horizontalInput, 0, 0));
             model.rotation = newRotation;
+
+            if (isMoving)
+            {
+                if (!audioSrc.isPlaying)
+                    audioSrc.Play();
+            }
         }
         else
         {
             animator.SetBool("isMoving", false);
+
+            isMoving = false;
+            if (!isMoving)
+            {
+                audioSrc.Stop();
+            }
         }
     }
 
